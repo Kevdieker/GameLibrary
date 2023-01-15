3eb8098ada62fd11ceac10a4b87b5de15abfad65
fatima204 <fatima.amrin.hossain@gmail.com>
2023-01-15 17:23:49 +0100

    normalplayer and extendedplayer score corrected

--- src/main/java/at/ac/fhcampuswien/gamelibrary/ropasc/NormalPlayers.java
+++ src/main/java/at/ac/fhcampuswien/gamelibrary/ropasc/NormalPlayers.java
@@ -109,11 +109,13 @@ public class NormalPlayers {
     public void player1Win() {
         result.setText("Player 1 wins!");
         player1Score.setText(String.valueOf(Integer.parseInt(player1Score.getText()) + 1));
+        player2Choice.equals("0");
     }
 
     public void player2Win() {
         result.setText("Player 2 wins!");
         player2Score.setText(String.valueOf(Integer.parseInt(player2Score.getText()) + 1));
+        player1Choice.equals("0");
     }
 
     private void checkIfWon() {
