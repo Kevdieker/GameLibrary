c5c3f0427fff3b977afe0631bf93b69044a82258
taha6 <taha.hagar@stud.fh-campuswien.ac.at>
2023-01-23 12:51:00 +0100

    Merge remote-tracking branch 'origin/master'

--- src/main/java/at/ac/fhcampuswien/gamelibrary/MainMenu.java
+++ src/main/java/at/ac/fhcampuswien/gamelibrary/MainMenu.java
@@ -3,9 +3,14 @@ package at.ac.fhcampuswien.gamelibrary;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 import java.io.IOException;
